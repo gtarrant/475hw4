14d1c22891b0b70f7e09c69528790bd884b76e43
OpenShift guest <gtarrant@umich.edu>
2014-03-21 16:22:59 -0400

    blah

--- modexp.cpp
+++ modexp.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 uberzahl modexp1(uberzahl n, uberzahl k, uberzahl m){
+// original modexp: n^k mod m
   uberzahl a(1);
   while(k > uberzahl(2)){
     if (k%2 == 0){
-      k = k/2;
+      k = k >> 1;
       n = (n*n)%m;
     }
     else{
@@ -18,6 +19,44 @@ uberzahl modexp1(uberzahl n, uberzahl k, uberzahl m){
   return (a*n*n)%m; 
 }
 
+// compute a^k mod pq using CRT
+uberzahl modexp2(uberzahl a, uberzahl k, uberzahl p, uberzahl q){
+    uberzahl a_p = modexp1(a, k % (p-uberzahl(1)), p);
+    uberzahl a_q = modexp1(a, k % (q-uberzahl(1)), q);
+    return a_p * q * q.inverse(p) + a_q * p * p.inverse(q);
+}
+
+uberzahl modexp3(uberzahl n, uberzahl a, uberzahl k, uberzahl M){
+    //Version with Montgomery Residues + Reductions
+    uberzahl r(1);
+    r = r << n;
+    
+    a = a * r % M;
+    
+    uberzahl rInverse;
+    rInverse = inverse(r);
+    
+    uberzahl multiplyStep(1);
+    
+    while( k > uberzhal(2)) {
+        if (k % 2 == 0) {
+            k = k >> 1;
+            a = (a*a*rInverse) % M;
+        }
+        
+        else {
+            k = k - 1;
+            multiplyStep = (multiplyStep*a*rInverse) % M;
+        }
+    }
+    return (multiplyStep*a*a*rInverse*rInverse*rInverse) % M;
+}
+
 int main(){
+    cout << modexp3(21, 13, 1023, 881*883)
   //cout << modexp1(uberzahl(2), uberzahl(10), uberzahl(1000)) << endl;
+}
+
+uberzahl Red(uberzahl T, uberzahl rInverse, uberzahl M) {
+    return T * rInverse % M;
 }
